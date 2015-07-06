10a871ce757bcf0b59d59aecc825878bb78dd62e
gurucharanhc <gurucharanhc@gmail.com>
2015-07-06 00:48:45 -0700

    Added DOM, SAX, STAX, JDOM and Simple XML parser

---
+++ 01_GettingStarted/XMLString/src/Main.java
@@ -0,0 +1,27 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import com.example.dataprovider.DataProvider;
+
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		String filename=DataProvider.DATADIR+"customers.xml";
+		
+		StringBuilder builder = new StringBuilder();
+		FileReader reader = new FileReader(new File(filename));
+		
+		int content;
+		while((content = reader.read())!=-1){
+			builder.append((char)content);
+		}
+		reader.close();
+		System.out.println(builder.toString());
+
+	}
+
+}
